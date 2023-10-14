05e6c9cd514e34077fc3910263c587c1a4c1b2b6
Boris Dovbysh <boris.dovbysh@gmail.com>
2023-10-14 13:47:14 +0300

    task done

---
+++ Calculator.java
@@ -0,0 +1,67 @@
+package seminars.first.Calculator;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+    // необходимые проверки для него используя граничные случаи
+    public static double squareRootExtraction(double num) {
+        //  0
+        //  Отрицательные числа
+        //  Дробные значения корней
+        //  Целые
+            if(num < 0) {
+                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+            }
+            return Math.sqrt(num);
+    }
+
+    // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
+    // Примерная сигнатура и тело метода:
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        // purchaseAmount - сумма покупки
+        // discountAmount - размер скидки
+        assertThat(purchaseAmount.isEqualTo(double));
+        assertThat(discountAmount.isEqualTo(int));
+        if (purchaseAmount < 0){
+            throw new Exception("Стоимость не может быть отрицательной");
+        }
+        if (discountAmount < 0) {
+            throw new Exception("Скидка не может быть отрицательной");
+        }
+        if (discountAmount > 100) {
+            throw new Exception("Скидка не может быть больше 100 %");
+        }
+        try {
+            if (purchaseAmount != double) or (discountAmount != int);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("ArithmeticException");
+        }
+        assert 98 == Calculator.calculatingDiscount(100, 2);
+        return purchaseAmount * (1 - (double) discountAmount / 100); // Метод должен возвращать сумму покупки со скидкой
+    }
+}
